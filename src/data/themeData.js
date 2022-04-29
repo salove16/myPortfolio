7d2be382464dca77b3e8cef847606fb17ddaf225
salove16 <shashank.shalabh909@gmail.com>
2022-04-30 00:50:26 +0530

    init

---
+++ src/data/themeData.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import { 
+    greenThemeLight, greenThemeDark, bwThemeLight, bwThemeDark, blueThemeLight, blueThemeDark, redThemeLight, redThemeDark, orangeThemeLight, orangeThemeDark, purpleThemeLight, purpleThemeDark, pinkThemeLight, pinkThemeDark, yellowThemeLight, yellowThemeDark
+} from '../theme/theme'
+
+
+export const themeData = {
+   theme_1: yellowThemeLight,
+   theme_2:yellowThemeDark,
+
+}
+
+
+// Choose theme from above
